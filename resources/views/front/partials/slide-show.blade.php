e3a51dbc5786ff930582677f7f92b7b3aeb06475
Michael Joyner <joyner.michael@gmail.com>
2019-10-10 12:43:52 +0800

    wip

--- resources/views/front/partials/slide-show.blade.php
+++ resources/views/front/partials/slide-show.blade.php
@@ -2,7 +2,7 @@
     <div class="relative slide-container">
         @foreach($slideshow as $slide)
             <div class="w-full h-full slideshow-slide relative">
-                <img src="{{ $slide['banner'] }}" class="w-full h-full object-cover opacity-0" style="transition: 3s;"
+                <img src="{{ $slide['banner'] }}" class="w-full h-full object-cover object-center opacity-0" style="transition: 3s;"
                      alt="">
                 <div class="absolute bg-tinted bottom-0 left-0 w-full  flex px-8 py-4">
                     <p class="w-full sm:w-2/5 leading-tight type-h1 text-white uppercase sm:mr-4 sm:pr-4 sm:border-r sm:border-white">
