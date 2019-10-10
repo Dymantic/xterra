e13f267cd39725a62090a3ee28b97213950c90c0
Michael Joyner <joyner.michael@gmail.com>
2019-10-10 12:01:46 +0800

    version assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,4 +18,5 @@ mix.js('resources/js/app.js', 'public/js')
    .less('resources/less/front.less', 'public/css')
     .options({
         postCss: [tailwindcss('./tailwind.config.js')]
-    });
+    })
+    .version();
