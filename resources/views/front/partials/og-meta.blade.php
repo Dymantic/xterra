cac55263850b47b1334c878cd2b7b33b2e7efc1a
Michael Joyner <joyner.michael@gmail.com>
2020-06-18 09:28:32 +0800

    update og tags to use property keyword

--- resources/views/front/partials/og-meta.blade.php
+++ resources/views/front/partials/og-meta.blade.php
@@ -1,8 +1,8 @@
-<meta name="og:image" content="{{ $ogImage ?? url('/images/XTERRA_FB_Page.jpg') }}"/>
-<meta name="og:url" content="{{ Request::url() }}"/>
-<meta name="og:title" content="{{ $ogTitle }}"/>
-<meta name="og:site_name" content="XTERRA TAIWAN Blog"/>
-<meta name="og:type" content="Website"/>
-<meta name="og:description" content="{{ $ogDescription }}"/>
-<meta name="description" content="{{ $ogDescription }}">
-<meta name="twitter:card" content="summary_large_image">
+<meta property="og:image" content="{{ $ogImage ?? url('/images/XTERRA_FB_Page.jpg') }}"/>
+<meta property="og:url" content="{{ Request::url() }}"/>
+<meta property="og:title" content="{{ $ogTitle }}"/>
+<meta property="og:site_name" content="XTERRA TAIWAN Blog"/>
+<meta property="og:type" content="Website"/>
+<meta property="og:description" content="{{ $ogDescription }}"/>
+<meta property="description" content="{{ $ogDescription }}">
+<meta property="twitter:card" content="summary_large_image">
