5066a161a4fca706dba2747f0ffaf1b536df680e
Michael Joyner <joyner.michael@gmail.com>
2020-08-06 17:52:42 +0800

    use srcset

--- resources/views/front/partials/slide-show.blade.php
+++ resources/views/front/partials/slide-show.blade.php
@@ -2,10 +2,10 @@
     <div class="relative slide-container">
         @foreach($slideshow as $slide)
             <div class="w-full h-full slideshow-slide relative">
-                <img  src="{{ $slide['banner'] }}" class="hidden md:block w-full h-full object-cover object-center opacity-0" style="transition: 3s;"
+                <img  src="{{ $slide['banner'] }}" class="w-full h-full object-cover object-center opacity-0" style="transition: 3s;" srcset="{{ $slide['banner_mobile'] }} 900w, {{ $slide['banner'] }} 1800w"
                      alt="">
-                <img src="{{ $slide['banner_mobile'] }}" class="md:hidden w-full h-full object-cover object-center opacity-0" style="transition: 3s;"
-                     alt="">
+{{--                <img src="{{ $slide['banner_mobile'] }}" class="md:hidden w-full h-full object-cover object-center opacity-0" style="transition: 3s;"--}}
+{{--                     alt="">--}}
                 <a href="{{ localUrl('/blog/' . $slide['slug']) }}">
                     <div class="absolute bg-tinted hover:bg-tinted-dark bottom-0 left-0 w-full  flex px-8 py-4">
                         <p class="w-full sm:w-2/5 leading-tight type-h1 text-white uppercase sm:mr-4 sm:pr-4 sm:border-r sm:border-white">
