0f205f71a80cc14156a8f19318133ea2e9c01b3c
Michael Joyner <joyner.michael@gmail.com>
2020-10-23 10:27:02 +0800

    wip

--- resources/views/front/events/sponsors.blade.php
+++ resources/views/front/events/sponsors.blade.php
@@ -3,21 +3,25 @@
         <p class="type-h2 mb-6 uppercase">Event Sponsors</p>
         <div class="flex flex-wrap justify-center">
             @foreach($event['sponsors'] as $sponsor)
-                <div class="w-64 flex flex-col justify-between shadow-small p-4 mx-6">
-                    <div>
-                        <div class="h-40 w-40 mx-auto">
-                            <img class="w-full h-full object-contain" src="{{ $sponsor['logo']['thumb'] }}" alt="{{ $sponsor['name'] }}">
-                        </div>
+                <div class="w-32 h-32 m-4">
+                    <a href="{{ $sponsor['link'] }}" target="_blank">
+                        <img class="w-full h-full object-contain" src="{{ $sponsor['logo']['thumb'] }}" alt="{{ $sponsor['name'] }}">
+                    </a>
 
-                        <p class="my-2 type-b2">{{ $sponsor['name'] }}</p>
+{{--                    <div>--}}
+{{--                        <div class="h-40 w-40 mx-auto">--}}
+{{--                            <img class="w-full h-full object-contain" src="{{ $sponsor['logo']['thumb'] }}" alt="{{ $sponsor['name'] }}">--}}
+{{--                        </div>--}}
 
-                        <p class="text-sm">{{ $sponsor['description'] }}</p>
-                    </div>
+{{--                        <p class="my-2 type-b2">{{ $sponsor['name'] }}</p>--}}
+
+{{--                        <p class="text-sm">{{ $sponsor['description'] }}</p>--}}
+{{--                    </div>--}}
 
 
-                    <div class="flex justify-end mt-6">
-                        <a target="_blank" href="{{ $sponsor['link'] }}" class="type-b2 text-blue-700 uppercase">Visit Sponsor &gt;</a>
-                    </div>
+{{--                    <div class="flex justify-end mt-6">--}}
+{{--                        <a target="_blank" href="{{ $sponsor['link'] }}" class="type-b2 text-blue-700 uppercase">Visit Sponsor &gt;</a>--}}
+{{--                    </div>--}}
                 </div>
             @endforeach
         </div>
