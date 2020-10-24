fe573e98e79d80463943d3ebef4cbf08d681e8de
Michael Joyner <joyner.michael@gmail.com>
2020-10-24 10:55:12 +0800

    wip

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -20,7 +20,7 @@
                        href="/top-secret/events">
                         <span>Events</span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             class="stroke-current h-4 ml-2 text-white">
+                             class="stroke-current h-4 ml-2 text-white hidden lg:block">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
                         </svg>
 
