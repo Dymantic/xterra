2e92213de6b85678c0b60782caf8c1753f9011ee
Michael Joyner <joyner.michael@gmail.com>
2020-10-24 11:02:10 +0800

    wip

--- resources/views/front/events/sponsor-logo-grid.blade.php
+++ resources/views/front/events/sponsor-logo-grid.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-wrap justify-center">
 @foreach($event['sponsors'] as $sponsor)
-    <div class="w-16 md:w-24 h-16 md:h-24 m-8">
+    <div class="w-16 md:w-24 h-16 md:h-24 m-4 md:m-8">
         <a href="{{ $sponsor['link'] }}" target="_blank">
             <img class="w-full h-full object-contain" src="{{ $sponsor['logo']['thumb'] }}" alt="{{ $sponsor['name'] }}">
         </a>
