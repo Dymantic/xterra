ba18480561a9cb48207218107aaf45f72f20a22d
Michael Joyner <joyner.michael@gmail.com>
2020-11-05 10:21:37 +0800

    table in editor js

--- resources/views/editorjs/prizes/table.blade.php
+++ resources/views/editorjs/prizes/table.blade.php
@@ -1,21 +1,23 @@
 @if(count($data) > 0)
     @php $headings = array_shift($data) @endphp
-<table class="w-full border border-red-700">
-    <thead>
-    <tr class="border-b border-red-700 text-white bg-red-700">
-        @foreach($headings as $heading)
-            <th class="p-2 text-left font-bold">{{ $heading }}</th>
-        @endforeach
-    </tr>
-    </thead>
-    <tbody class="text-sm">
-    @foreach($data as $row)
-    <tr class="@if($loop->odd) bg-gray-200 @endif">
-        @foreach($row as $cell)
-        <td class="py-1 px-2">{{ $cell }}</td>
-        @endforeach
-    </tr>
-    @endforeach
-    </tbody>
-</table>
+    <div class="overflow-x-auto">
+        <table class="w-full border border-red-700">
+            <thead>
+            <tr class="border-b border-red-700 text-white bg-red-700">
+                @foreach($headings as $heading)
+                    <th class="p-2 text-left font-bold">{{ $heading }}</th>
+                @endforeach
+            </tr>
+            </thead>
+            <tbody class="text-sm">
+            @foreach($data as $row)
+                <tr class="@if($loop->odd) bg-gray-200 @endif">
+                    @foreach($row as $cell)
+                        <td class="py-1 px-2">{{ $cell }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endif
--- resources/views/editorjs/races/table.blade.php
+++ resources/views/editorjs/races/table.blade.php
@@ -1,11 +1,14 @@
-<table class="w-full border border-red-700">
-    <tbody>
-    @foreach($data as $row)
-    <tr class="@if($loop->even) bg-gray-100 @endif">
-        @foreach($row as $cell)
-        <td class="px-2 py-1">{!! $cell !!}</td>
+<div class="overflow-x-auto">
+    <table class="w-full border border-red-700">
+        <tbody>
+        @foreach($data as $row)
+            <tr class="@if($loop->even) bg-gray-100 @endif">
+                @foreach($row as $cell)
+                    <td class="px-2 py-1">{!! $cell !!}</td>
+                @endforeach
+            </tr>
         @endforeach
-    </tr>
-    @endforeach
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
+
