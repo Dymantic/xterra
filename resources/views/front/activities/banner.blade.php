333465dbe8d1f7bcdf3acc912e447e526e8f9c93
Michael Joyner <joyner.michael@gmail.com>
2020-11-05 09:18:41 +0800

    activity banner mobile

--- resources/views/front/activities/banner.blade.php
+++ resources/views/front/activities/banner.blade.php
@@ -1,21 +1,24 @@
-<div class="min-h-banner-tall md:min-h-banner bg-cover flex flex-col justify-center pl-3 md:pl-32 pr-3" style="background-image: url({{ $activity['title_image']['banner'] }})">
-    <div class="max-w-xl w-full mx-auto lg:mx-0 bg-tinted-dark p-4 rounded leading-tight">
-        <div class="flex justify-between items-center my-2 py-2 border-b-2 border-red-500">
+<div class="event-banner min-h-banner-tall md:min-h-banner bg-cover flex flex-col justify-center sm:pl-3 md:pl-32 sm:pr-3">
+
+    <img src="{{ $activity['title_image']['card'] }}" alt="{{ $activity['name'] }}" class="block sm:hidden" style="width: 100vw; height: 100vw; object-fit: cover;">
+
+    <div class="max-w-xl w-full mx-auto lg:mx-0 bg-tinted-dark p-4 sm:rounded leading-tight">
+        <div class="hidden sm:flex justify-between items-center my-2 py-2 border-b-2 border-red-500">
             <p class="text-white type-h2">{{ $activity['distance'] }}</p>
             @include('svg.event-categories.' . $activity['category'], ['classes' => 'h-8 text-white mr-4 md:ml-4 md:mr-0'])
         </div>
-        <p class="type-h0 uppercase text-white my-4">{{ $activity['name'] }}</p>
-        <div class="flex items-center">
-            @include('svg.icons.location', ['classes' => 'h-6 text-red-500 mr-2'])
-            <p class="type-h1 text-white">{{ $activity['venue_name'] ?: $activity['location'] }}</p>
+        <p class="body-font font-bold text-2xl sm:text-6xl uppercase text-white my-3 sm:my-4">{{ $activity['name'] }}</p>
+        <div class="flex items-center mb-2 sm:mb-0">
+            @include('svg.icons.location', ['classes' => 'h-5 sm:h-6 text-red-500 mr-2'])
+            <p class="font-heading font-medium text-lg sm:text-4xl uppercase sm:capitalize text-white">{{ $activity['venue_name'] ?: $activity['location'] }}</p>
         </div>
         <div class="flex items-center">
-            @include('svg.icons.calendar', ['classes' => 'h-6 text-red-500 mr-2'])
-            <p class="type-h1 text-white">{{ $activity['date'] }}</p>
+            @include('svg.icons.calendar', ['classes' => 'h-5 sm:h-6 text-red-500 mr-2'])
+            <p class="font-heading font-medium text-lg sm:text-4xl uppercase sm:capitalize text-white">{{ $activity['date'] }}</p>
         </div>
         <div class="text-right mt-4">
             @if($activity['registration_link'])
-            <a href="{{ $activity['registration_link'] }}" target="_blank" class="type-b2 text-white hover:text-red-700 uppercase">{{ trans('activities.register_now') }} &gt;</a>
+            <a href="{{ $activity['registration_link'] }}" target="_blank" class="type-b3 sm:type-b2 text-white hover:text-red-700 sm:uppercase">{{ trans('activities.register_now') }} &gt;</a>
                 @endif
         </div>
     </div>
--- resources/views/front/activities/show.blade.php
+++ resources/views/front/activities/show.blade.php
@@ -1,5 +1,19 @@
 @extends('front.base', ['flickity' => true, 'all_scripts' => false, 'has_promo_video' => true])
 
+@section('head')
+    <style>
+        .event-banner {
+            background-image: url({{ $activity['title_image']['banner']}});
+        }
+
+        @media screen and (max-width: 639px) {
+            .event-banner {
+                background-image: none;
+            }
+        }
+    </style>
+@endsection
+
 @section('content')
     @include('front.activities.banner')
     @include('front.activities.activity-nav')
