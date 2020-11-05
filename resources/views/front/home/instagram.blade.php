9cc51e446ecb6c35ee8a7f6303dda0bfa802e171
Michael Joyner <joyner.michael@gmail.com>
2020-11-05 09:08:45 +0800

    fix instagram

--- resources/views/front/home/instagram.blade.php
+++ resources/views/front/home/instagram.blade.php
@@ -1,11 +1,11 @@
 @if(count($ig_posts))
 <div>
-    <div class="flex">
+    <div class="flex items-stretch">
         <div class="w-half-screen h-half-screen flex-shrink-0 relative group">
             @include('front.home.ig-post', ['ig_post' => $ig_posts[0]])
         </div>
-        <div>
-            <div class="flex">
+        <div class="flex flex-col">
+            <div class="flex flex-1">
                 <div class="w-1/2 group relative">
                     @include('front.home.ig-post', ['ig_post' => $ig_posts[1]])
                 </div>
@@ -13,7 +13,7 @@
                     @include('front.home.ig-post', ['ig_post' => $ig_posts[2]])
                 </div>
             </div>
-            <div class="flex">
+            <div class="flex flex-1">
                 <div class="w-1/2 group relative">
                     @include('front.home.ig-post', ['ig_post' => $ig_posts[3]])
                 </div>
