b8babd4abacf202e0a971f76b002518cf5156fc8
Michael Joyner <joyner.michael@gmail.com>
2020-11-09 11:09:26 +0800

    wip

--- resources/views/editorjs/campaigns/illustratedText.blade.php
+++ resources/views/editorjs/campaigns/illustratedText.blade.php
@@ -4,6 +4,6 @@
         <p class="mt-3">{!! $text !!}</p>
     </div>
     <div class="w-full md:w-1/2 mt-10 md:mt-0 {{ $align === 'right' ? 'md:order-2 md:ml-6' : 'md:order-1 md:mr-6' }}">
-        <img src="{{ $image_src }}" alt="">
+        <img src="{{ is_array($image_src) ? '' : $image_src }}" alt="">
     </div>
 </div>
