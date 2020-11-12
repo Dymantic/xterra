0799eba07c34ef5e58fe58d9a6d1ad1f6da7189d
Michael Joyner <joyner.michael@gmail.com>
2020-11-12 17:15:01 +0800

    new google tracking code

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -6,20 +6,21 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title', 'XTERRA TAIWAN')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="alternate" hreflang="{{ app()->getLocale() === 'en' ? 'zh' : 'en' }}" href="{{ url(transUrl(Request::path())) }}">
+    <link rel="alternate" hreflang="{{ app()->getLocale() === 'en' ? 'zh' : 'en' }}"
+          href="{{ url(transUrl(Request::path())) }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-{{--    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500|Source+Sans+Pro:400,400i,600,700|Source+Serif+Pro" rel="stylesheet">--}}
+    {{--    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500|Source+Sans+Pro:400,400i,600,700|Source+Serif+Pro" rel="stylesheet">--}}
 
-    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500|Source+Sans+Pro:400,700|Source+Serif+Pro" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500|Source+Sans+Pro:400,700|Source+Serif+Pro"
+          rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.min.js" defer></script>
 
     @if($flickity ?? false)
-    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     @endif
-
 
 
     <link rel="stylesheet" href="{{ mix('css/front.css') }}">
@@ -32,13 +33,23 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#2e2d2f">
 
-    @yield('head')
+@yield('head')
 
 <!-- Google Tag Manager -->
 
-    <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{{ config('services.google.tag_manager_code') }}');
-    </script>
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-W7XMWRB');</script>
 
     <!-- End Google Tag Manager -->
 
@@ -47,11 +58,13 @@
 <body class="h-full pt-16 font-body leading-relaxed {{ $bodyClasses ?? '' }} antialiased">
 <!-- Google Tag Manager (noscript) -->
 <noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRWNH4X" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7XMWRB"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
 <!--[if lte IE 9]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
 <![endif]-->
 <div class="h-full flex flex-col">
     <div id="app" class="flex-1">
@@ -71,6 +84,7 @@
 
         var player;
         window.promoVideoIsReady = false;
+
         function onYouTubeIframeAPIReady() {
             window.promoVideo = new YT.Player('home-promo-video', {
                 events: {
@@ -78,11 +92,11 @@
                 }
             });
 
-            window.playPromoVideo = function() {
+            window.playPromoVideo = function () {
                 window.promoVideo.playVideo();
             }
 
-            window.pausePromoVideo = function() {
+            window.pausePromoVideo = function () {
                 window.promoVideo.pauseVideo();
             }
         }
@@ -93,33 +107,41 @@
     </script>
 @endif
 <script>
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
         FB.init({
-            appId      :  {{ config('services.facebook.app_id') }},
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v4.0'
+            appId:  {{ config('services.facebook.app_id') }},
+            cookie: true,
+            xfbml: true,
+            version: 'v4.0'
         });
 
         FB.AppEvents.logPageView();
 
     };
 
-    (function(d, s, id){
+    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
         js.src = "https://connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 </script>
 @if($all_scripts ?? true)
-<script src="{{ mix("js/front.js") }}"></script>
+    <script src="{{ mix("js/front.js") }}"></script>
 @endif
 <script src="{{ mix("js/front_basic.js") }}"></script>
 <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', '{{ config('services.google.analytics_id') }}', 'auto'); ga('send', 'pageview')
+    window.ga = function () {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', '{{ config('services.google.analytics_id') }}', 'auto');
+    ga('send', 'pageview')
 </script>
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
