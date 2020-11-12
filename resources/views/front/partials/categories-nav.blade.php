23b4a33b53d3d06dbe0061a86b086966d3f03948
Michael Joyner <joyner.michael@gmail.com>
2020-11-12 08:42:41 +0800

    blog home link to blog

--- resources/views/front/partials/categories-nav.blade.php
+++ resources/views/front/partials/categories-nav.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center type-h2 bg-red-700 px-4 lg:flex whitespace-no-wrap justify-center py-1 uppercase overflow-auto">
     <div class="mx-12 inline-block">
         <a class="text-white hover:underline {{ in_array(Request::path(), ['en', 'zh']) ? 'underline' : 'no-underline' }}"
-           href="{{ localUrl('/') }}">{{ trans('navbar.all') }}</a>
+           href="{{ localUrl('/blog') }}">{{ trans('navbar.all') }}</a>
 
     </div>
     @foreach($categories as $category)
